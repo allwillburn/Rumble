fbf2442e79199a84d4ff008463205e18b669f7f9
allwillburn <livefortoday470@gmail.com>
2016-06-25 13:29:54 -0400

    Update Rumble.lua

--- Rumble.lua
+++ Rumble.lua
@@ -75,7 +75,7 @@ OnTick(function (myHero)
 	--AUTO LEVEL UP
 	if RumbleMenu.AutoMode.Level:Value() then
 
-			spellorder = {_Q, _E, _W, _Q, _E, _R, _Q, _W, _W, _Q, _R, _Q, _W, _E, _E, _R, _E, _E}
+			spellorder = {_Q, _E, _W, _Q, _Q, _R, _Q, _W, _Q, _W, _R, _E, _E, _W, _W, _R, _E, _E}
 			if GetLevelPoints(myHero) > 0 then
 				LevelSpell(spellorder[GetLevel(myHero) + 1 - GetLevelPoints(myHero)])
 			end
